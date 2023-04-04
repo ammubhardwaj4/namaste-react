322392e118cbf230faf81eecc52b49bb832bd021
Amit Pathak <45812131+ammubhardwaj4@users.noreply.github.com>
2023-04-04 17:10:13 +0000

    added work for episode 5

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import logo from "../../logo.png";
+
+
+const Header = () => {
+    return (
+      <div className="header">
+        <div className="logo-container">
+          <img className="logo" src={logo}></img>
+        </div>
+        <div className="nav-items">
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact Us</li>
+            <li>Cart</li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
+  export default Header;
